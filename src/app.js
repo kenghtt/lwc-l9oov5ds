7481f4442d5883f471aebf8bd51537526d6707fb
kenghtt <25125291+undefined@users.noreply.github.com>
2022-10-26 04:57:27 +0000

    Random Commit Console Log

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { LightningElement } from "lwc";
 export default class App extends LightningElement {
   handleShowModal() {
     const modal = this.template.querySelector("c-modal");
+    console.log("test123")
     modal.show();
   }
 }
